c84b7d604ac821f543764d627b55c395a585c68e
melikhovaeva <sshultse@vk.com>
2023-11-28 13:48:56 +0300

    done pagination

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# Getting Started with Create React App
-
+# Melikhova Eva-Sofia 221-322 group
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,50 @@
-import React from 'react';
-import { Table } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Button, Table } from 'antd';
+import axios from 'axios';
 import './App.css';
+import type { ColumnsType } from 'antd/es/table';
 
-function App() {
-  const dataSource = [
+const App = () => {
+  interface DataType {
+    country: string,
+    name: string,
+  }
+
+  const columns: ColumnsType<DataType> = [
     {
-      key: '1',
-      name: 'Mike',
-      age: 32,
-      address: '10 Downing Street',
-      job: 'Developer',
+      title: 'Страна',
+      dataIndex: 'country',
+      key: 'country',
     },
     {
-      key: '2',
-      name: 'John',
-      age: 42,
-      address: '10 Downing Street',
-      job: 'Engineer',
-    },
-    {
-      key: '3',
-      name: 'Alice',
-      age: 28,
-      address: '5th Avenue',
-      job: 'Designer',
-    },
-    {
-      key: '4',
-      name: 'Bob',
-      age: 35,
-      address: 'Sunset Boulevard',
-      job: 'Manager',
-    },
-    {
-      key: '5',
-      name: 'Eve',
-      age: 29,
-      address: 'Broadway',
-      job: 'Analyst',
-    },
-    {
-      key: '6',
-      name: 'Charlie',
-      age: 45,
-      address: 'Main Street',
-      job: 'Consultant',
-    },
-    {
-      key: '7',
-      name: 'Grace',
-      age: 31,
-      address: 'Elm Street',
-      job: 'Teacher',
-    },
-    {
-      key: '8',
-      name: 'David',
-      age: 39,
-      address: 'Maple Avenue',
-      job: 'Doctor',
-    },
-  ];
-  
-  const columns = [
-    {
-      title: 'Name',
+      title: 'Название школы',
       dataIndex: 'name',
       key: 'name',
     },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-    {
-      title: 'Job',
-      dataIndex: 'job',
-      key: 'job',
-    },
-  ];
-  
-  return (
-    <div>
-      <Table dataSource={dataSource} columns={columns} />;
-    </div>
-  );
-}
+  ]
 
+  const [offset, setOffset] = useState<number>(1);
+  const [dataSource, setDataSource] = useState<DataType[]>();
+
+
+  const getUniversity = async (offset: number, limit: number) => {
+    const response = await axios.get(`http://universities.hipolabs.com/search?offset=${offset}&limit=${limit}`)
+    setDataSource(response.data);    
+  }
+
+  const LIMIT_LIST_SCHOOL = 10
+
+  useEffect(() => {
+    getUniversity(offset, LIMIT_LIST_SCHOOL)
+  }, [offset])
+
+  return (
+    <>
+      <Table dataSource={dataSource} columns={columns} pagination={false}/>
+      <Button onClick={() => setOffset(offset - 1)} disabled={!offset}>Назад</Button>
+      <Button onClick={() => setOffset(offset + 1)}>Вперед</Button>
+      <p>Текущая страница: {offset + 1}</p>
+    </>
+  )
+}
 export default App;
