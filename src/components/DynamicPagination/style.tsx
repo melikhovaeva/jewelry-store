bdf052598480b9340569cc796266e71811fcced0
Melikhova Eva-Sofia <sshultse@vk.com>
2024-03-07 15:54:14 +0300

    done pagination

---
+++ src/components/DynamicPagination/style.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const BlockOnserver = styled.div`
+  height: 40px;
+  background-color: #FFFFF;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  color: #000000;
+`;
+
+export const DynamicPaginationContainer = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  background-color: #FFFFF;
+`;
+
+export const LoadingText = styled.div`
+  text-align: center;
+  font-size: 18px;
+  margin: 20px 0;
+  color: #777;
+`;
