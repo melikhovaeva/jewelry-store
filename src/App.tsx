1ce9a985cd5925f5cebfa6dfaa2ca101c116ce3e
Melikhova Eva-Sofia <sshultse@vk.com>
2024-03-28 18:18:31 +0300

    Добавлен EsLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { AppRoute } from './components';
-import Root from './pages/Root/root';
-import University from './pages/University/university';
-import Catalog from './pages/Catalog/catalog';
-import Navbar from './components/Navbar/navbar';
-import Blog from './pages/Blog';
-import Story from './pages/Story/story';
+import React, { useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { AppRoute } from "./components";
+import Root from "./pages/Root/root";
+import University from "./pages/University/university";
+import Catalog from "./pages/Catalog/catalog";
+import Navbar from "./components/Navbar/navbar";
+import Blog from "./pages/Blog";
+import Story from "./pages/Story/story";
 
 export function App(): React.ReactElement {
   const [isAuth, setIsAuth] = useState<boolean>(false);
@@ -15,33 +15,13 @@ export function App(): React.ReactElement {
     <>
       <Navbar isAuth={isAuth} setIsAuth={setIsAuth} />
       <Routes>
-        <Route 
-          path={AppRoute.Root}
-          element={<Root />}
-        />
-        <Route 
-          path={AppRoute.University}
-          element={<University />}
-        />
-        <Route 
-          path={AppRoute.Catalog} 
-          element={<Catalog />}
-        />
-        <Route 
-          path={AppRoute.Blog} 
-          element={<Blog />}
-        />
-        <Route 
-          path={AppRoute.Story} 
-          element={<Story />}
-        />
-        <Route 
-          path='*'
-          element={<Navigate to={'/'} replace />}
-        />
+        <Route path={AppRoute.Root} element={<Root />} />
+        <Route path={AppRoute.University} element={<University />} />
+        <Route path={AppRoute.Catalog} element={<Catalog />} />
+        <Route path={AppRoute.Blog} element={<Blog />} />
+        <Route path={AppRoute.Story} element={<Story />} />
+        <Route path="*" element={<Navigate to={"/"} replace />} />
       </Routes>
     </>
   );
 }
-
-
