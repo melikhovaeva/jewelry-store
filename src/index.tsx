b0591193d9d27f48f1e6e2fd4d32fa0575f90903
Melikhova Eva-Sofia <sshultse@vk.com>
2024-04-08 19:00:06 +0300

    3 лаба

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './App';
-import { BrowserRouter } from 'react-router-dom';
-import GlobalStyles from './global-styles'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { BrowserRouter } from "react-router-dom";
+import GlobalStyles from "./global-styles";
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-  <GlobalStyles/>
+    <GlobalStyles />
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
 );
